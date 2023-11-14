23923bb230cf320a25fafbcd26cd936508a78d9f
MengqianXu <116844816+MengqianXu@users.noreply.github.com>
2023-11-14 15:18:39 +0100

    Update Time.h

--- Projet_Algo/Time.h
+++ Projet_Algo/Time.h
@@ -1,11 +1,6 @@
 #ifndef TIME_H
 #define TIME_H
 
-// 任务的结构
-struct Task {
-    int taskNumber;
-    int executionTime;
-};
 
 int calculateCompletionTime(struct Graph* graph, int v, int* visited, struct Task* tasks, int* completionTimes);
 int calculateTotalCompletionTime(struct Graph* graph, struct Task* tasks);
