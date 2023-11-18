845bb16944e3ca4eda70a695a67db39bf05f1370
YasmineTU <129085556+YasmineTU@users.noreply.github.com>
2023-11-18 14:53:41 +0100

    Update Main.c

--- Projet_Algo/Main.c
+++ Projet_Algo/Main.c
@@ -39,3 +39,35 @@ int main() {
 
     return 0;
 }
+
+
+
+
+
+//TU main
+int main() {
+    // 创建一个图，假设有5个顶点
+    int numVertices = 5;
+    struct Graph* myGraph = initializeGraph(numVertices);
+
+    if (myGraph == NULL) {
+        printf("Graph initialization failed.\n");
+        return 1; // 退出程序，表示出错
+    }
+
+    // 添加一些边
+    addEdge(myGraph, 0, 1);
+    addEdge(myGraph, 1, 2);
+    addEdge(myGraph, 2, 3);
+    addEdge(myGraph, 3, 4);
+    addEdge(myGraph, 4, 0);
+
+    // 打印邻接矩阵
+    printf("Adjacency Matrix:\n");
+    printGraph(myGraph);
+
+    // 释放图的内存
+    freeGraph(myGraph);
+
+    return 0; // 表示程序成功执行
+}
