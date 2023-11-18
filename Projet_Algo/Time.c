b225e5fbd797f2e1c1f8ef4d1ba65b476c37eae8
YasmineTU <129085556+YasmineTU@users.noreply.github.com>
2023-11-18 17:49:01 +0100

    Update Time.c

--- Projet_Algo/Time.c
+++ Projet_Algo/Time.c
@@ -49,3 +49,57 @@ int calculateTotalCompletionTime(struct Graph* graph, struct Task* tasks) {
 
     return totalCompletionTime;
 }
+
+
+//Time TUUU
+
+// DFS递归函数（用于动态规划）
+int calculateCompletionTime(struct Graph* graph, int v, int* visited, struct Task* tasks, int* completionTimes) {
+    if (visited[v]) {
+        return completionTimes[v];
+    }
+
+    visited[v] = 1;
+
+    int maxTime = 0;
+
+    for (int i = 0; i < graph->vertices; ++i) {
+        if (graph->adjacencyMatrix[v][i] == 1) {
+            int time = tasks[v].executionTime + calculateCompletionTime(graph, i, visited, tasks, completionTimes);
+            maxTime = (time > maxTime) ? time : maxTime;
+        }
+    }
+
+    completionTimes[v] = maxTime;
+    return maxTime;
+}
+
+// 计算总完成时间
+int calculateTotalCompletionTime(struct Graph* graph, struct Task* tasks) {
+    int* visited = (int*)malloc(graph->vertices * sizeof(int));
+    int* completionTimes = (int*)malloc(graph->vertices * sizeof(int));
+
+    for (int i = 0; i < graph->vertices; ++i) {
+        visited[i] = 0;
+        completionTimes[i] = -1;  // 初始化为-1，表示未计算
+    }
+
+    int totalCompletionTime = 0;
+
+    // 对每个顶点应用动态规划
+    // tu 这里我们需要对图进行拓扑排序，然后按照拓扑排序的顺序访问每个顶点
+    int* topoOrder = topologicalSort(graph);
+    for (int i = 0; i < graph->vertices; ++i) {
+        int v = topoOrder[i];
+        if (!visited[v]) {
+            totalCompletionTime += calculateCompletionTime(graph, v, visited, tasks, completionTimes);
+        }
+    }
+
+
+    free(visited);
+    free(completionTimes);
+    free(topoOrder);
+
+    return totalCompletionTime;
+}
